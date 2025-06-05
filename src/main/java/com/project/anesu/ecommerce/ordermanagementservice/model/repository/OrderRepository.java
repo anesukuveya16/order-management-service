58f349a1a72f12dbc308dc70158b551a9f4173c8
anesukuveya <anesugail16@gmail.com>
2025-06-05 02:07:56 +0200

    minor code refactor in OrderRepository

--- src/main/java/com/project/anesu/ecommerce/ordermanagementservice/model/repository/OrderRepository.java
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/model/repository/OrderRepository.java
@@ -2,13 +2,12 @@ package com.project.anesu.ecommerce.ordermanagementservice.model.repository;
 
 import com.project.anesu.ecommerce.ordermanagementservice.entity.order.Order;
 import com.project.anesu.ecommerce.ordermanagementservice.entity.order.OrderStatus;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Optional<Order> findByIdAndStatus(Long orderId, OrderStatus status);
+  Optional<Order> findByIdAndOrderStatus(Long orderId, OrderStatus status);
 }
