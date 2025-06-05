b344588ed49a245ff283311362903f10a78e2415
anesukuveya <anesugail16@gmail.com>
2025-06-05 02:08:31 +0200

    update service implementation classes.

--- src/main/java/com/project/anesu/ecommerce/ordermanagementservice/service/CustomerServiceImpl.java
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/service/CustomerServiceImpl.java
@@ -4,31 +4,42 @@ import com.project.anesu.ecommerce.ordermanagementservice.entity.customer.Custom
 import com.project.anesu.ecommerce.ordermanagementservice.model.CustomerService;
 import com.project.anesu.ecommerce.ordermanagementservice.model.repository.CustomerRepository;
 import com.project.anesu.ecommerce.ordermanagementservice.service.exception.CustomerNotFoundException;
-
 import java.util.List;
 import java.util.Optional;
 
+import com.project.anesu.ecommerce.ordermanagementservice.service.util.CustomerValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
 
   private final CustomerRepository customerRepository;
+  private final CustomerValidator customerValidator;
 
-  private static final String CUSTOMER_NOT_FOUND_MESSAGE = "Customer not found";
-
-  public CustomerServiceImpl(CustomerRepository customerRepository) {
-    this.customerRepository = customerRepository;
-  }
+  private static final String CUSTOMER_NOT_FOUND_MESSAGE = "Customer not found.";
 
   @Override
   public Customer createCustomer(Customer customer) {
+
+    customerValidator.validate(customer);
     return customerRepository.save(customer);
   }
 
   @Override
-  public Customer updateCustomer(Long customerId, Customer updatedCustomer) {
-    return null;
+  public Customer updateCustomer(Long customerId, Customer updatedCustomer) throws CustomerNotFoundException {
+
+    Customer existingCustomer = customerRepository.findById(customerId)
+            .orElseThrow(() -> new CustomerNotFoundException(CUSTOMER_NOT_FOUND_MESSAGE));
+
+    existingCustomer.setEmail(updatedCustomer.getEmail());
+    existingCustomer.setFirstName(updatedCustomer.getFirstName());
+    existingCustomer.setLastName(updatedCustomer.getLastName());
+    existingCustomer.setBirthDate(updatedCustomer.getBirthDate());
+    existingCustomer.setPhoneNumber(updatedCustomer.getPhoneNumber());
+
+    return customerRepository.save(existingCustomer);
   }
 
   @Override
@@ -48,4 +59,5 @@ public class CustomerServiceImpl implements CustomerService {
     }
     customerRepository.deleteById(customerId);
   }
+
 }
--- src/main/java/com/project/anesu/ecommerce/ordermanagementservice/service/OrderServiceImpl.java
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/service/OrderServiceImpl.java
@@ -1,46 +1,78 @@
 package com.project.anesu.ecommerce.ordermanagementservice.service;
 
 import com.project.anesu.ecommerce.ordermanagementservice.entity.order.Order;
+import com.project.anesu.ecommerce.ordermanagementservice.entity.order.OrderItem;
 import com.project.anesu.ecommerce.ordermanagementservice.entity.order.OrderStatus;
 import com.project.anesu.ecommerce.ordermanagementservice.model.OrderService;
 import com.project.anesu.ecommerce.ordermanagementservice.model.repository.OrderRepository;
-import com.project.anesu.ecommerce.ordermanagementservice.service.exception.CustomerNotFoundException;
 import com.project.anesu.ecommerce.ordermanagementservice.service.exception.OrderNotFoundException;
-
+import com.project.anesu.ecommerce.ordermanagementservice.service.exception.ValidationFailedException;
+import com.project.anesu.ecommerce.ordermanagementservice.service.util.OrderValidator;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
+import java.util.*;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-  private final OrderRepository orderRepository;
   private final RestTemplate restTemplate;
-  private final  String stockServiceBaseUrl = "http://localhost:8081";
-
+  private final OrderValidator orderValidator;
+  private final OrderRepository orderRepository;
+  private static final String VALIDATE_ORDER = "http://localhost:8081";
   private static final String ORDER_NOT_FOUND_EXCEPTION_MESSAGE = "Order not found";
 
   @Override
-  public Order createOrder(Order order) throws OrderNotFoundException{
+  public Order createOrder(Order order, List<OrderItem> orderItems) throws OrderNotFoundException {
 
+    validateNewOrder(order, orderItems);
 
+    List<Map<String, Object>> batchInventoryValidationRequest = new ArrayList<>();
+    for (OrderItem orderItem : orderItems) {
+      Map<String, Object> orderItemData = new HashMap<>();
+      orderItemData.put("productId", orderItem.getProductId());
+      orderItemData.put("quantity", orderItem.getQuantity());
+      batchInventoryValidationRequest.add(orderItemData);
+    }
 
-    //
+    String validationUrl = VALIDATE_ORDER + "/api/stock/validate-and-deduct-product";
 
+    try {
+      ResponseEntity<String> validationResponse =
+          restTemplate.postForEntity(validationUrl, batchInventoryValidationRequest, String.class);
+
+      // the service itself could not process the order entirely
+      if (validationResponse.getStatusCode() != HttpStatus.OK) {
+        throw new IllegalStateException("An error occurred while validating order.");
+      }
+      // while making the request of validation
+    } catch (HttpClientErrorException e) {
+      throw new ValidationFailedException(
+          "Order validation has failed" + e.getResponseBodyAsString());
+    } catch (RestClientException e) {
+      throw new RestClientException("Failed to connect to Product Service" + e.getMessage());
+    }
+
+    order.setCustomerId(order.getCustomerId());
     order.setOrderDate(LocalDateTime.now());
     order.setOrderStatus(OrderStatus.PROCESSING);
 
-    return null;
+   for (OrderItem orderItem : orderItems) {
+     orderItem.setOrder(order);
+   }
+   order.setOrderItem(orderItems);
+
+    return orderRepository.save(order);
   }
 
-
   @Override
-  public Order processPendingOrder( Long orderId, OrderStatus status) throws OrderNotFoundException {
+  public Order processPendingOrder(Long orderId, OrderStatus status) throws OrderNotFoundException {
 
     Order orderRequest = getOrderByIdAndStatus(orderId, OrderStatus.PROCESSING);
     orderRequest.setOrderStatus(OrderStatus.OUT_FOR_DELIVERY);
@@ -48,7 +80,8 @@ public class OrderServiceImpl implements OrderService {
   }
 
   @Override
-  public Order markOrderAsDelivered(Long orderId, OrderStatus status) throws OrderNotFoundException {
+  public Order markOrderAsDelivered(Long orderId, OrderStatus status)
+      throws OrderNotFoundException {
 
     Order orderRequest = getOrderByIdAndStatus(orderId, OrderStatus.OUT_FOR_DELIVERY);
     orderRequest.setOrderStatus(OrderStatus.DELIVERED);
@@ -61,31 +94,48 @@ public class OrderServiceImpl implements OrderService {
     return orderRepository.findById(customerId);
   }
 
-  public Order getOrderByIdAndStatus(Long orderId, OrderStatus status)  throws OrderNotFoundException {
+  public Order getOrderByIdAndStatus(Long orderId, OrderStatus status)
+      throws OrderNotFoundException {
 
-    return orderRepository.findByIdAndStatus(orderId, status)
-            .orElseThrow(() -> new OrderNotFoundException(ORDER_NOT_FOUND_EXCEPTION_MESSAGE + orderId));
+    return orderRepository
+        .findByIdAndOrderStatus(orderId, status)
+        .orElseThrow(() -> new OrderNotFoundException(ORDER_NOT_FOUND_EXCEPTION_MESSAGE + orderId));
   }
 
   @Override
   public List<Order> getAllOrders() {
+
     return orderRepository.findAll();
   }
 
   @Override
-  public Order updateOrder(Long orderId, Order updatedOrder) {
-    return null;
+  public Order updateOrder(Long orderId, Order updatedOrder) throws OrderNotFoundException {
+
+    Order existingOrder = orderRepository.findById(orderId).
+            orElseThrow(() -> new OrderNotFoundException(ORDER_NOT_FOUND_EXCEPTION_MESSAGE + orderId));
+
+    existingOrder.setOrderDate(updatedOrder.getOrderDate());
+    existingOrder.setOrderItem(updatedOrder.getOrderItem());
+    existingOrder.setTotalPrice(updatedOrder.getTotalPrice());
+    existingOrder.setOrderStatus(updatedOrder.getOrderStatus());
+
+    return orderRepository.save(existingOrder);
   }
 
   @Override
   public void cancelOrder(Long orderId, String cancellationReason) throws OrderNotFoundException {
 
-    Order orderRequest = orderRepository.findById(orderId)
+    Order orderRequest =
+        orderRepository
+            .findById(orderId)
             .orElseThrow(() -> new OrderNotFoundException(ORDER_NOT_FOUND_EXCEPTION_MESSAGE));
 
     orderRequest.setOrderStatus(OrderStatus.CANCELLED);
     orderRequest.setCancellationReason(cancellationReason);
     orderRepository.save(orderRequest);
+  }
 
+  private void validateNewOrder(Order order, List<OrderItem> orderItems) {
+    orderValidator.validateNewOrder(order, orderItems);
   }
 }
