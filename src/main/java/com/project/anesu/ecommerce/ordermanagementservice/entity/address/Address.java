b909e257ec79f6af8b9fa7d12bbaa93293da533e
anesukuveya <anesugail16@gmail.com>
2025-06-07 10:30:27 +0200

    add Address entity and repository.

---
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/entity/address/Address.java
@@ -0,0 +1,36 @@
+package com.project.anesu.ecommerce.ordermanagementservice.entity.address;
+
+import com.project.anesu.ecommerce.ordermanagementservice.entity.customer.Customer;
+import com.project.anesu.ecommerce.ordermanagementservice.entity.order.Order;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String streetName;
+    private String streetNumber;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
---
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/model/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.project.anesu.ecommerce.ordermanagementservice.model.repository;
+
+import com.project.anesu.ecommerce.ordermanagementservice.entity.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
